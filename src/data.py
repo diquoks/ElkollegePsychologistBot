b43ed29c3dd88c72af999d231fa23dc67acebacf
Denis Titovets <den232titovets@yandex.ru>
2025-11-17 22:42:26 +0300

    Инициализация репозитория

---
+++ src/data.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+import pyquoks, aiogram, aiogram.fsm.state
+
+
+# region Providers
+
+class StringsProvider(pyquoks.data.StringsProvider):
+    class MenuStrings(pyquoks.data.StringsProvider.Strings):
+        @property
+        def start(self) -> str:
+            return (
+                "Привет, психолог скоро ответит\n"
+                "Можешь описать свою проблему подробней?"
+            )
+
+        @property
+        def psychologist(self) -> str:
+            return (
+                "Для обратной связи с пользователем\n"
+                "ответьте на пересланное сообщение!"
+            )
+
+    _OBJECTS = {
+        "menu": MenuStrings,
+    }
+
+    menu: MenuStrings
+
+
+# endregion
+
+# region Managers
+
+class ConfigManager(pyquoks.data.ConfigManager):
+    class SettingsConfig(pyquoks.data.ConfigManager.Config):
+        _SECTION = "Settings"
+
+        _VALUES = {
+            "bot_token": str,
+            "file_logging": bool,
+            "psychologist_id": int,
+            "skip_updates": bool,
+        }
+
+        bot_token: str
+        file_logging: bool
+        psychologist_id: int
+        skip_updates: bool
+
+    _OBJECTS = {
+        "settings": SettingsConfig,
+    }
+
+    settings: SettingsConfig
+
+
+# endregion
+
+# region Services
+
+class LoggerService(pyquoks.data.LoggerService):
+    def log_user_interaction(self, user: aiogram.types.User, interaction: str) -> None:
+        user_info = f"@{user.username} ({user.id})" if user.username else user.id
+        self.info(f"{user_info} - \"{interaction}\"")
+
+# endregion
